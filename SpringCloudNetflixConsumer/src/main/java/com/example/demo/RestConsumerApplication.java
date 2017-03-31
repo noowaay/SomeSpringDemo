3b8a5ffee2fcf86833b4e850c6930e5a80448909
tto7 <tto7@csc.com>
2017-03-31 18:15:09 +0700

    add spring cloud

---
+++ SpringCloudNetflixConsumer/src/main/java/com/example/demo/RestConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+public class RestConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestConsumerApplication.class, args);
+	}
+}
